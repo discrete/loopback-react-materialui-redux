d45cfba0e6efe796c819b78ba4e660c50bcaf05d
Kim, Joohong <joohong.kim@kkaeal.kr>
2017-12-12 07:55:05 +0900

    Add react-router-dom for navigation https://github.com/ReactTraining/react-router

--- client_src/src/index.js
+++ client_src/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+ReactDOM.render(<BrowserRouter>
+<div>
+  <Route exact path="/" component={App} />
+  <Route exact path="/home" component={Home} />
+</div>
+</BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
