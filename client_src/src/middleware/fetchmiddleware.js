b9ddfc746ba9ea1f931a4408e200584514aa6ec2
Joohong Kim <joohong.kim@kkaeal.kr>
2017-12-20 22:55:00 +0900

    promise middleware를 이용해서 처리힘

--- client_src/src/middleware/fetchmiddleware.js
+++
@@ -1,8 +0,0 @@
-export const PENDING = 'PENDING';
-export const FULFILLED = 'FULFILLED';
-export const REJECTED = 'REJECTED';
-const defaultTypes = [PENDING, RESOLVED, REJECTED];
-
-export default function fetchMiddleware(config ={}) {
-
-}
